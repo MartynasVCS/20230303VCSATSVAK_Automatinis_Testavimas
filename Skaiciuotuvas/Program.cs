9a727beadbac3e58a5e4d2745565961962e6143b
MartynasVCS <martynasvcs@gmail.com>
2023-04-20 22:39:47 +0300

    Add Skaiciuotuvas app and demo unit test

---
+++ Skaiciuotuvas/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Skaiciuotuvas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Skaiciuotuvas.Dalyba(1, 3) == 0.3333333333333)
+            {
+                Console.WriteLine("Passed");
+            }
+            else
+            {
+                Console.WriteLine("Failed");
+            }
+        }
+    }
+
+    public class Skaiciuotuvas
+    {
+        public static double Dalyba(int skaicius1, int skaicius2)
+        {
+            return (double)skaicius1 / skaicius2;
+        }
+
+        internal static int Sandauga(int skaicius1, int skaicius2)
+        {
+            return skaicius1 * skaicius2;
+        }
+
+        internal static int Skirtumas(int skaicius1, int skaicius2)
+        {
+            return skaicius1 - skaicius2;
+        }
+
+        internal static int Suma(int skaicius1, int skaicius2)
+        {
+            return skaicius1 + skaicius2;
+        }
+    }
+}
---
+++ UnitTestai/SkaiciuotuvoTestai.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace UnitTestai
+{
+    public class SkaiciuotuvoTestai
+    {
+        [Test]
+        public void Dalyba_1_3()
+        {
+            double expectedResult = 0.33333333333333333;
+
+            double actualResult = Skaiciuotuvas.Skaiciuotuvas.Dalyba(1, 3);
+            
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
