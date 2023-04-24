af0aa3989b653e6cb690bf1f3dabe8f0a7f0b6be
MartynasVCS <martynasvcs@gmail.com>
2023-04-24 20:22:34 +0300

    Add changes to calculator program

--- Skaiciuotuvas/Program.cs
+++ Skaiciuotuvas/Program.cs
@@ -24,19 +24,49 @@ namespace Skaiciuotuvas
             return (double)skaicius1 / skaicius2;
         }
 
-        internal static int Sandauga(int skaicius1, int skaicius2)
+        public static int Sandauga(int skaicius1, int skaicius2)
         {
             return skaicius1 * skaicius2;
         }
 
-        internal static int Skirtumas(int skaicius1, int skaicius2)
+        public static double SandaugaDu(double skaicius1, double skaicius2)
+        {
+            return skaicius1 * skaicius2;
+        }
+
+        public static double SandaugaTrys(double skaicius1, double skaicius2, double skaicius3)
+        {
+            return skaicius1 * skaicius2 * skaicius3;
+        }
+
+        public static int Skirtumas(int skaicius1, int skaicius2)
         {
             return skaicius1 - skaicius2;
         }
 
-        internal static int Suma(int skaicius1, int skaicius2)
+        public static int Suma(int skaicius1, int skaicius2)
         {
             return skaicius1 + skaicius2;
+        }
+
+        public static double SumaDu(double skaicius1, double skaicius2)
+        {
+            return skaicius1 + skaicius2;
+        }
+
+        public static double SumaTrys(double skaicius1, double skaicius2, double skaicius3)
+        {
+            return skaicius1 + skaicius2 + skaicius3;
+        }
+
+        public static double SumaTrysRounded(double skaicius1, double skaicius2, double skaicius3)
+        {
+            return Math.Round(skaicius1 + skaicius2 + skaicius3, 5);
+        }
+
+        public static decimal SumaTrysDecimal(decimal skaicius1, decimal skaicius2, decimal skaicius3)
+        {
+            return skaicius1 + skaicius2 + skaicius3;
         }
     }
 }
