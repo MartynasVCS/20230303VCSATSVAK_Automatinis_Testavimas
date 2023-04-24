3991c1d3bc49a40ae0bb4eb13ab223f04975af9b
MartynasVCS <martynasvcs@gmail.com>
2023-04-24 20:24:14 +0300

    Add additional test cases

--- UnitTestai/SkaiciuotuvoTestai.cs
+++ UnitTestai/SkaiciuotuvoTestai.cs
@@ -4,12 +4,43 @@ namespace UnitTestai
 {
     public class SkaiciuotuvoTestai
     {
+        
         [Test]
-        public void Dalyba_1_3()
+        public void Suma_01_01()
         {
-            double expectedResult = 0.33333333333333333;
+            double expectedResult = 0.2;
 
-            double actualResult = Skaiciuotuvas.Skaiciuotuvas.Dalyba(1, 3);
+            double actualResult = Skaiciuotuvas.Skaiciuotuvas.SumaDu(0.1, 0.1);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Suma_01_01_01()
+        {
+            double expectedResult = 0.3;
+
+            double actualResult = Skaiciuotuvas.Skaiciuotuvas.SumaTrys(0.1, 0.1, 0.1);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Suma_01_01_01_Rounded()
+        {
+            double expectedResult = 0.3;
+
+            double actualResult = Skaiciuotuvas.Skaiciuotuvas.SumaTrysRounded(0.1, 0.1, 0.1);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Suma_01_01_01_Decimal()
+        {
+            decimal expectedResult = 0.3m;
+
+            decimal actualResult = Skaiciuotuvas.Skaiciuotuvas.SumaTrysDecimal(0.1m, 0.1m, 0.1m);
             
             Assert.AreEqual(expectedResult, actualResult);
         }
