3bd3cd2bde7687ca772007bd44baa23c463bcb0b
MartynasVCS <martynasvcs@gmail.com>
2023-04-24 22:46:43 +0300

    Add demo test for Google

---
+++ SeleniumTestsWithoutPOM/Google.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTestsWithoutPOM
+{
+    public class Google
+    {
+        [Test]
+        public void OpenGoogle()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.google.com/";
+
+            //IWebElement buttonRejectCookies = driver.FindElement(By.Id("W0wltc"));
+            //buttonRejectCookies.Click();
+
+            IWebElement buttonRejectCookies = driver.FindElement(By.XPath("(//*[@id='CXQnmb']//button)[3]"));
+            buttonRejectCookies.Click();
+        }
+    }
+}
