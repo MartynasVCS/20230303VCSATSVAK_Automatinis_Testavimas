89ad97516811af9f84890161a256f899b674fed2
MartynasVCS <martynasvcs@gmail.com>
2023-04-24 20:24:40 +0300

    Add example for test case

--- UnitTestai/SkaiciuotuvoTestai.cs
+++ UnitTestai/SkaiciuotuvoTestai.cs
@@ -4,6 +4,19 @@ namespace UnitTestai
 {
     public class SkaiciuotuvoTestai
     {
+        [TestCase(1, 3, 0.33333333333333333)]
+        [TestCase(10, 2, 5)]
+        [TestCase(8, 4, 2)]
+        [TestCase(9, 3, 3)]
+        [TestCase(8, 2, 4)]
+        [TestCase(10, 5, 2)]
+        public void Dalyba(int number1, int number2, double expectedResult)
+        {
+            double actualResult = Skaiciuotuvas.Skaiciuotuvas.Dalyba(number1, number2);
+            System.Threading.Thread.Sleep(4000);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
         
         [Test]
         public void Suma_01_01()
