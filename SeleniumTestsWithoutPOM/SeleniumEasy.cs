1bf01b1e05e4457f04fb1ff65e11ca5158856862
MartynasVCS <martynasvcs@gmail.com>
2023-04-27 20:40:52 +0300

    Add demo code for selenium easy single input form

---
+++ SeleniumTestsWithoutPOM/SeleniumEasy.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Safari;
+using System;
+
+namespace SeleniumTestsWithoutPOM
+{
+    internal class SeleniumEasy
+    {
+        [Test]
+        public void BasicFirstFormDemoSingleInputField()
+        {
+            string expectedResult = "Martynas";
+            string tekstas = Console.ReadLine();
+
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://demo.seleniumeasy.com/basic-first-form-demo.html";
+
+            IWebElement inputEnterMessage = driver.FindElement(By.XPath("//*[@id='get-input']//*[@id='user-message']"));
+            inputEnterMessage.SendKeys(expectedResult);
+            
+            IWebElement buttonShowMessage = driver.FindElement(By.XPath("//*[@id='get-input']//button"));
+            buttonShowMessage.Click();
+            
+            IWebElement textYourMessage = driver.FindElement(By.Id("display"));
+            string actualResult = textYourMessage.Text;
+
+            Assert.AreEqual(expectedResult, actualResult);
+
+            driver.Quit();
+        }
+    }
+}
