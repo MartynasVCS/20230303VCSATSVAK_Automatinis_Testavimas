e7c0c36d9df20b3b4f382142112724a45ff2c110
MartynasVCS <martynasvcs@gmail.com>
2023-04-27 20:14:36 +0300

    Add demo code for Vilnius Coding School address assert

--- SeleniumTestsWithoutPOM/Google.cs
+++ SeleniumTestsWithoutPOM/Google.cs
@@ -7,16 +7,51 @@ namespace SeleniumTestsWithoutPOM
     public class Google
     {
         [Test]
-        public void OpenGoogle()
+        public void VilniusCodingSchoolAddress()
         {
+            string expectedResult = "Žalgirio gatvė 90,\r\nA korpusas, 2 aukštas,\r\nVilnius";
+
+            // Atidarome svetainę
             IWebDriver driver = new ChromeDriver();
             driver.Url = "https://www.google.com/";
 
-            //IWebElement buttonRejectCookies = driver.FindElement(By.Id("W0wltc"));
-            //buttonRejectCookies.Click();
-
+            // Uždarome cookies langą
             IWebElement buttonRejectCookies = driver.FindElement(By.XPath("(//*[@id='CXQnmb']//button)[3]"));
             buttonRejectCookies.Click();
+
+            // Įvedame tekstą į paieškos laukelį
+            IWebElement inputSearch = driver.FindElement(By.Name("q"));
+            inputSearch.SendKeys("Vilnius coding school");
+
+            // Paspaudžiame ant Google logotipo, kad uždarytume suggestions langą kuris užstoja paieškos mygtuką
+            IWebElement imageGoogle = driver.FindElement(By.ClassName("lnXdpd"));
+            imageGoogle.Click();
+
+            // Paspaudžiame paieškos mygtuką
+            IWebElement buttonSearch = driver.FindElement(By.XPath("(//*[@name='btnK'])[2]"));
+            buttonSearch.Click();
+
+            // Paspaudžiame ant pirmo rezultato (ne reklaminio)
+            IWebElement linkFirstResult = driver.FindElement(By.XPath("//*[@id='res']//a"));
+            linkFirstResult.Click();
+
+            // Vilnius Coding School svetainėje uždarome reklamą
+            IWebElement buttonCloseAd = driver.FindElement(By.XPath("//*[@aria-label='Close']"));
+            // Palaukiame kol reklama atsiras
+            System.Threading.Thread.Sleep(2000);
+            buttonCloseAd.Click();
+            // Palaukiame kol reklama išnyks
+            System.Threading.Thread.Sleep(2000);
+
+            // Nuskaitome Vilniaus mokyklos adresą
+            IWebElement textVilniusSchoolAddress = driver.FindElement(By.XPath("//*[@id='kontaktai']//h3[text()='VILNIUS CODING SCHOOL']/following-sibling::div/p"));
+            string actualResult = textVilniusSchoolAddress.Text;
+
+            // Palyginame reikšmes
+            StringAssert.Contains(expectedResult, actualResult);
+
+            // Uždarome naršyklę
+            driver.Quit();
         }
     }
 }
