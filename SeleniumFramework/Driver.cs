e901659b89f9748f1b9c729a7ccd47c13780720f
MartynasVCS <martynasvcs@gmail.com>
2023-05-04 19:23:06 +0300

    Add driver class methods

--- SeleniumFramework/Driver.cs
+++ SeleniumFramework/Driver.cs
@@ -1,6 +1,30 @@
-﻿namespace SeleniumFramework
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumFramework
 {
     internal class Driver
     {
+        private static IWebDriver driver;
+
+        internal static void InitializeDriver()
+        {
+            driver = new ChromeDriver();
+        }
+
+        internal static IWebDriver GetDriver()
+        {
+            return driver;
+        }
+
+        internal static void OpenPage(string url)
+        {
+            driver.Url = url;
+        }
+
+        internal static void ShutdownDriver()
+        {
+            driver.Quit();
+        }
     }
 }
